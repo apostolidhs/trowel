b16a9ece90d85ccebd5b34dad95bfec2e91ae3ef
Giannhs Apostolidhs <giannhs.apostolidhs@gmail.com>
2015-12-25 01:52:58 +0000

    Lexical analyzer character stream unit tests

--- test/units/CharacterStream.spec.ts
+++ test/units/CharacterStream.spec.ts
@@ -1,23 +1,35 @@
 /// <reference path="../../tsDefinitions/tsd.d.ts" />
-/// <reference path="../../src/lexical/CharacterStream.ts" />
+/// <reference path="../../src/frontend/lexical/CharacterStream.ts" />
+/// <reference path="../utilities/testUtilities.ts" />
 
 describe('js source text read as characters stream', function() {
 
 	it('every character in the stream read properly', function() {
 
-		var files: _.Dictionary<string> = (<any>window).__html__;
+		const charStreamFiles = trl.test.Utilities.getSampleFiles((<any>window).__html__, ["/charStream"]);
 
-		_.each(files, (src: string, filename: string) => {
+		_.each(charStreamFiles, (content: string, filepath: string) => {
 
-			var cs = new trl.lexical.CharacterStream(src);
-			var char, cursor = 0;
+			let cs = new trl.frontend.lexical.CharacterStream(content);
+			let char, cursor = 0;
 			while (char = cs.getNextChar()) {
-				var offset = cs.getCurrOffset();
-				expect(char).toEqual(src.charCodeAt(cursor++));
-				expect(offset).toEqual(cursor);
+				trl.test.Utilities.expectToEqual(char, content.charCodeAt(cursor), "each char from a stream should equal with the corresponding char from the source");
+				++cursor;
+				trl.test.Utilities.expectToEqual(cs.getCursor(), cursor, "each char offset from a stream should equal with the corresponding char offset from the source");
+				const nextChar = cs.getNextChar();
+				if(nextChar !== undefined) {
+					cs.bwdCursor();
+					trl.test.Utilities.expectToEqual(cs.getCursor(), cursor, "bwdCursor should decrease the coursor by one");
+					cs.bwdCursor();
+					trl.test.Utilities.expectToEqual(cs.isEof(), false, "current char cannot be end of file");
+					trl.test.Utilities.expectToEqual(char, cs.getChar(), "bwdCursor of getNextChar is the same character");
+					cs.getNextChar();
+					let token = cs.tokenize(cs.getCursor()-1);
+					trl.test.Utilities.expectToEqual(token, String.fromCharCode(char), "tokenized string is equal with the current char");
+				}	
 			}
-			expect(char).toEqual(undefined);
-			expect(src.length).toEqual(cs.getCurrOffset());
+			trl.test.Utilities.expectToEqual(char, undefined, "on stream finish next value is undefined");
 		});
+		console.log("finish");
 	});
 });

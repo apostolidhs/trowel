eb43859f0d30f1a4f31012c94ebddeb63fa3780f
Giannhs Apostolidhs <giannhs.apostolidhs@gmail.com>
2016-02-14 01:20:28 +0000

    update the usage of the lexer in the api

--- src/frontend/api.ts
+++ src/frontend/api.ts
@@ -1,29 +1,24 @@
 /// <reference path="../../tsDefinitions/tsd.d.ts" />
 /// <reference path="lexical/Lexer.ts" />
-/// <reference path="lexical/Exception.ts" />
+/// <reference path="utilities/Exception.ts" />
 /// <reference path="lexical/CharacterStream.ts" />
 
 module trl.frontend.api {
 
 	interface ITokenizeResult {
 		tokens: lexical.IToken[],
-		exceptions?: lexical.IException[]
+		exceptions?: utilities.IException[]
 	}
 
-	export function tokenize(src: string): ITokenizeResult {
-		let cs = new lexical.CharacterStream(src),
-			eh = new lexical.ExceptionHandler(),
-			lex = new lexical.Lexer(cs, eh);
+	export function tokenize(src: string, options?: lexical.ILexerOptions): ITokenizeResult {
+		const cs = new lexical.CharacterStream(src),
+			eh = new utilities.ExceptionHandler(),
+			lex = new lexical.Lexer(cs, eh, options);
 
-		let tokens: lexical.IToken[] = [];
-		while (true) {
-			let token = lex.getNextToken();
-			if (token) {
-				tokens.push(token);
-			}
-			else {
-				break;
-			}
+		const tokens: lexical.IToken[] = [];
+		while (lex.hasNext()) {
+			const token = lex.nextToken();
+            tokens.push(token);
 		};
 
 		let tokenizeResult: ITokenizeResult = {

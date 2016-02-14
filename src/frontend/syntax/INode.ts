3f74763bb5e241e4daf255ca1388a2ccd3478f4b
Giannhs Apostolidhs <giannhs.apostolidhs@gmail.com>
2016-02-14 01:19:09 +0000

    ecmascript 5 AST nodes as defined in MDN

---
+++ src/frontend/syntax/INode.ts
@@ -0,0 +1,263 @@
+/// <reference path="../lexical/ILexer.ts" />
+
+module trl.frontend.syntax {
+
+    export interface INode {
+        type: string;
+        loc: lexical.ITokenSourceLocation
+    }
+
+    export interface IProgram extends INode {
+        body: IStatement[];
+    }
+
+    export interface IFunction extends INode {
+        id?: IIdentifier;
+        params: string[];
+        body: IBlockStatement;
+    }
+
+    export interface IStatement extends INode { }
+
+    export interface IEmptyStatement extends IStatement { }
+
+    export interface IBlockStatement extends IStatement {
+        body: IStatement[];
+    }
+
+    export interface IExpressionStatement extends IStatement {
+        expression: IExpression;
+    }
+
+    export interface IIfStatement extends IStatement {
+        test: IExpression;
+        consequent: IStatement;
+        alternate?: IStatement;
+    }
+
+    export interface IBreakStatement extends IStatement {
+        label?: IIdentifier;
+    }
+
+    export interface IContinueStatement extends IStatement {
+        label?: IIdentifier;
+    }
+
+    export interface ISwitchStatement extends IStatement {
+        discriminant: IExpression;
+        cases: ISwitchCase[];
+    }
+
+    export interface IReturnStatement extends IStatement {
+        argument?: IExpression;
+    }
+
+    export interface IThrowStatement extends IStatement {
+        argument: IExpression;
+    }
+
+    export interface ITryStatement extends IStatement {
+        block: IBlockStatement;
+        handler?: ICatchClause;
+        finalizer?: IBlockStatement;
+    }
+
+    export interface IWhileStatement extends IStatement {
+        test: IExpression;
+        body: IStatement;
+    }
+
+    export interface IDoWhileStatement extends IStatement {
+        body: IStatement;
+        test: IExpression;
+    }
+
+    export interface IForStatement extends IStatement {
+        init?: IVariableDeclaration | IExpression;
+        test?: IExpression;
+        update?: IExpression;
+        body: IStatement;
+    }
+
+    export interface IForInStatement extends IStatement {
+        left: IVariableDeclaration | IExpression;
+        right: IExpression;
+        body: IStatement;
+        each: boolean;
+    }
+    
+    export interface IDebuggerStatement extends IStatement { }
+
+    export interface IDeclaration extends IStatement { }
+
+    export interface IFunctionDeclaration extends IFunction, IDeclaration { }
+
+    export interface IVariableDeclaration extends IDeclaration {
+        declarations: IVariableDeclarator[];
+        kind: string; //"var"
+    }
+
+    export interface IVariableDeclarator extends INode {
+        id: string;
+        init?: IExpression;
+    }
+
+    export interface IExpression extends INode { }
+
+    export interface IThisExpression extends IExpression { }
+
+    export interface IArrayExpression extends IExpression {
+        elements?: IExpression[];
+    }
+
+    export interface IObjectExpression extends IExpression {
+        properties: IProperty[];
+    }
+
+    export interface IProperty extends INode {
+        key: ILiteral | IIdentifier;
+        value: IExpression;
+        kind: string; //"init" | "get" | "set"
+    }
+    
+    export interface IFunctionExpression extends IFunction, IExpression { } 
+
+    export interface ISequenceExpression extends IExpression {
+        expressions: IExpression[];
+    }
+
+    export interface IUnaryExpression extends IExpression {
+        operator: string; //IUnaryOperator;
+        prefix: boolean;
+        argument: IExpression;
+    }
+
+    export interface IBinaryExpression extends IExpression {
+        operator: string; //IBinaryOperator;
+        left: IExpression;
+        right: IExpression;
+    }
+
+    export interface IAssignmentExpression extends IExpression {
+        operator: string; //IAssignmentOperator;
+        left: IExpression;
+        right: IExpression;
+    }
+
+    export interface IUpdateExpression extends IExpression {
+        operator: string; //IUpdateOperator;
+        argument: IExpression;
+        prefix: boolean;
+    }
+
+    export interface ILogicalExpression extends IExpression {
+        operator: string; //ILogicalOperator;
+        left: IExpression;
+        right: IExpression;
+    }
+
+    export interface IConditionalExpression extends IExpression {
+        test: IExpression;
+        alternate: IExpression;
+        consequent: IExpression;
+    }
+
+    export interface INewExpression extends IExpression {
+        callee: IExpression;
+        arguments: IExpression[];
+    }
+
+    export interface ICallExpression extends IExpression {
+        callee: IExpression;
+        arguments: IExpression[];
+    }
+
+    export interface IMemberExpression extends IExpression {
+        object: IExpression;
+        property: IIdentifier | IExpression;
+        computed: boolean;
+    }
+
+    export interface ISwitchCase extends INode {
+        test?: IExpression;
+        consequent: IStatement[];
+    }
+
+    export interface ICatchClause extends INode {
+        param: string;
+        body: IBlockStatement;
+    }
+
+    export interface IIdentifier extends INode {
+        name: string;
+    }
+
+    export interface ILiteral extends INode {
+        value: string | boolean | number | IRegExp;
+    }
+    
+    export interface IRegExp { }
+
+    // const t = true;
+
+    // export const IUnaryOperator = {
+    //     "-": t,
+    //     "+": t,
+    //     "!": t,
+    //     "~": t,
+    //     "typeof": t,
+    //     "void": t,
+    //     "delete": t
+    // };
+
+    // export const IBinaryOperator = {
+    //     "==": t,
+    //     "!=": t,
+    //     "===": t,
+    //     "!==": t,
+    //     "<": t,
+    //     "<=": t,
+    //     ">": t,
+    //     ">=": t,
+    //     "<<": t,
+    //     ">>": t,
+    //     ">>>": t,
+    //     "+": t,
+    //     "-": t,
+    //     "*": t,
+    //     "/": t,
+    //     "%": t,
+    //     "|": t,
+    //     "^": t,
+    //     "&": t,
+    //     "in": t,
+    //     "instanceof": t,
+    //     "..": t
+    // };
+
+    // export const ILogicalOperator = {
+    //     "||": t,
+    //     "&&": t
+    // };
+
+    // export const IAssignmentOperator = {
+    //     "=": t,
+    //     "+=": t,
+    //     "-=": t,
+    //     "*=": t,
+    //     "/=": t,
+    //     "%=": t,
+    //     "<<=": t,
+    //     ">>=": t,
+    //     ">>>=": t,
+    //     "|=": t,
+    //     "^=": t,
+    //     "&=": t
+    // };
+
+    // export const IUpdateOperator = {
+    //     "++": t,
+    //     "--": t
+    // };
+    
+}
---
+++ src/frontend/syntax/NodeFactory.ts
@@ -0,0 +1,322 @@
+/// <reference path="INode.ts" />
+
+module trl.frontend.syntax {
+    
+    export class NodeFactory {
+        
+        constructor(private enablePos: boolean) {}
+        
+        private createNode(node, loc?: lexical.ITokenSourceLocation) {
+            if(this.enablePos) {
+                node.loc = loc;
+            } 
+            return node;
+        }
+        
+        public createProgram(body: IStatement[], loc?: lexical.ITokenSourceLocation): IProgram {
+            return this.createNode({
+                type: "Program",
+                body
+            }, loc);
+        }      
+        
+        public createStatementEmpty(loc?: lexical.ITokenSourceLocation): IEmptyStatement {
+            return this.createNode({
+                type: "EmptyStatement"
+            }, loc);            
+        }
+        
+        public createStatementBlock(body: IStatement[], loc?: lexical.ITokenSourceLocation): IBlockStatement {
+            return this.createNode({
+                type: "BlockStatement",
+                body
+            }, loc);               
+        }
+        
+        public createStatementExpression(expression: IExpression, loc?: lexical.ITokenSourceLocation): IExpressionStatement {
+            return this.createNode({
+                type: "ExpressionStatement",
+                expression
+            }, loc);               
+        }
+        
+        public createStatementIf(test: IExpression, consequent: IStatement, alternate?: IStatement, loc?: lexical.ITokenSourceLocation): IIfStatement {
+            return this.createNode({
+                type: "IfStatement",
+                test,
+                consequent,
+                alternate
+            }, loc); 
+        }
+        
+        public createStatementBreak(label: IIdentifier, loc?: lexical.ITokenSourceLocation): IBreakStatement {
+            return this.createNode({
+                type: "BreakStatement",
+                label
+            }, loc); 
+        }
+        
+        public createStatementContinue(label: IIdentifier, loc?: lexical.ITokenSourceLocation): IContinueStatement {
+            return this.createNode({
+                type: "ContinueStatement",                
+                label
+            }, loc); 
+        }
+        
+        public createStatementSwitch(discriminant: IExpression, cases: ISwitchCase[], loc?: lexical.ITokenSourceLocation): ISwitchStatement {
+            return this.createNode({
+                type: "SwitchStatement",                
+                discriminant,
+                cases
+            }, loc);
+        }
+        
+        public createStatementReturn(argument: IExpression, loc?: lexical.ITokenSourceLocation): IReturnStatement {
+            return this.createNode({
+                type: "ReturnStatement",                
+                argument
+            }, loc);
+        }
+        
+        public createStatementThrow(argument: IExpression, loc?: lexical.ITokenSourceLocation): IThrowStatement {
+            return this.createNode({
+                type: "ThrowStatement",                
+                argument
+            }, loc);
+        }
+        
+        public createStatementTry(block: IBlockStatement, handler: ICatchClause, finalizer: IBlockStatement, loc?: lexical.ITokenSourceLocation): ITryStatement {
+            return this.createNode({
+                type: "TryStatement",                
+                block,
+                handler,
+                finalizer
+            }, loc);
+        }
+        
+        public createStatementWhile(test: IExpression, body: IStatement, loc?: lexical.ITokenSourceLocation): IWhileStatement {
+            return this.createNode({
+                type: "WhileStatement",                
+                test,
+                body
+            }, loc);
+        }
+        
+        public createStatementDoWhile(body: IStatement, test: IExpression, loc?: lexical.ITokenSourceLocation): IDoWhileStatement {
+            return this.createNode({
+                type: "DoWhileStatement",                
+                body,
+                test
+            }, loc);
+        }
+        
+        public createStatementFor(init: IVariableDeclaration | IExpression, test: IExpression, update: IExpression, body: IStatement, loc?: lexical.ITokenSourceLocation): IForStatement {
+            return this.createNode({
+                type: "ForStatement",                
+                init,
+                test,
+                update,
+                body
+            }, loc);
+        }
+        
+        public createStatementForIn(left: IVariableDeclaration | IExpression, right: IExpression, body: IStatement, each: boolean, loc?: lexical.ITokenSourceLocation): IForInStatement {
+            return this.createNode({
+                type: "ForInStatement",                
+                left,
+                right,
+                body,
+                each
+            }, loc);
+        }
+        
+        public createStatementDebugger(loc?: lexical.ITokenSourceLocation): IDebuggerStatement {
+            return this.createNode({
+                type: "ForInStatement",
+                loc
+            }, loc);
+        }
+        
+        public createDeclarationFunction(id: IIdentifier, params: string[], body: IBlockStatement, declarations: IVariableDeclarator[], loc?: lexical.ITokenSourceLocation): IFunctionDeclaration {
+            return this.createNode({
+                type: "FunctionDeclaration",                
+                id,
+                params,
+                body
+            }, loc);
+        }
+        
+        public createDeclarationVariable(declarations: IVariableDeclarator[], loc?: lexical.ITokenSourceLocation): IVariableDeclaration {
+            return this.createNode({
+                type: "VariableDeclaration",                
+                declarations,
+                kind: "var"
+            }, loc);
+        }
+        
+        public createVariableDeclarator(id: string, init: IExpression, loc?: lexical.ITokenSourceLocation): IVariableDeclarator {
+            return this.createNode({
+                type: "VariableDeclarator",                
+                id,
+                init
+            }, loc);
+        }
+        
+        public createExpressionThis(loc?: lexical.ITokenSourceLocation): IThisExpression {
+            return this.createNode({
+                type: "ThisExpression",
+                loc
+            }, loc);
+        }
+        
+        public createExpressionArray(elements: IExpression[], loc?: lexical.ITokenSourceLocation): IArrayExpression {
+            return this.createNode({
+                type: "ArrayExpression",                
+                elements
+            }, loc);
+        }
+        
+        public createExpressionObject(properties: IProperty[], loc?: lexical.ITokenSourceLocation): IObjectExpression {
+            return this.createNode({
+                type: "ObjectExpression",                
+                properties
+            }, loc);
+        }
+        
+        public createObjectProperty(key: ILiteral | IIdentifier, value: IExpression, kind: string, loc?: lexical.ITokenSourceLocation): IProperty {
+            return this.createNode({
+                type: "Property",                
+                key,
+                value,
+                kind
+            }, loc);
+        }
+        
+        public createExpressionFunction(id: IIdentifier, params: string[], body: IBlockStatement, loc?: lexical.ITokenSourceLocation): IFunctionExpression {
+            return this.createNode({
+                type: "FunctionExpression",                
+                id,
+                params,
+                body
+            }, loc);
+        }               
+        
+        public createExpressionSequence(expressions: IExpression[], loc?: lexical.ITokenSourceLocation): ISequenceExpression {
+            return this.createNode({
+                type: "SequenceExpression",                
+                expressions
+            }, loc);
+        }
+        
+        public createExpressionUnary(operator: string, prefix: boolean, argument: IExpression, loc?: lexical.ITokenSourceLocation): IUnaryExpression {
+            return this.createNode({
+                type: "UnaryExpression",                
+                operator,
+                prefix,
+                argument
+            }, loc);
+        }
+        
+        public createExpressionBinary(operator: string, left: IExpression, right: IExpression, loc?: lexical.ITokenSourceLocation): IBinaryExpression {
+            return this.createNode({
+                type: "BinaryExpression",                
+                operator,
+                left,
+                right
+            }, loc);
+        }
+        
+        public createExpressionAssignment(operator: string, left: IExpression, right: IExpression, loc?: lexical.ITokenSourceLocation): IAssignmentExpression {
+            return this.createNode({
+                type: "AssignmentExpression",                
+                operator,
+                left,
+                right
+            }, loc);
+        }
+        
+        public createExpressionUpdate(operator: string, argument: IExpression, prefix: boolean, loc?: lexical.ITokenSourceLocation): IUpdateExpression {
+            return this.createNode({
+                type: "UpdateExpression",                
+                operator,
+                argument,
+                prefix
+            }, loc);
+        }
+        
+        public createExpressionLogical(operator: string, left: IExpression, right: IExpression, loc?: lexical.ITokenSourceLocation): ILogicalExpression {
+            return this.createNode({
+                type: "LogicalExpression",                
+                operator,
+                left,
+                right
+            }, loc);
+        }
+        
+        public createExpressionConditional(test: IExpression, alternate: IExpression, consequent: IExpression, loc?: lexical.ITokenSourceLocation): IConditionalExpression {
+            return this.createNode({
+                type: "ConditionalExpression",                
+                test,
+                alternate,
+                consequent
+            }, loc);
+        }
+        
+        public createExpressionNew(callee: IExpression, args: IExpression[], loc?: lexical.ITokenSourceLocation): INewExpression {
+            return this.createNode({
+                type: "NewExpression",                
+                callee,
+                arguments: args
+            }, loc);
+        }
+        
+        public createExpressionCall(callee: IExpression, args: IExpression[], loc?: lexical.ITokenSourceLocation): ICallExpression {
+            return this.createNode({
+                type: "CallExpression",                
+                callee,
+                arguments: args
+            }, loc);
+        }
+        
+        public createExpressionMember(object: IExpression, property: IIdentifier | IExpression, computed: boolean, loc?: lexical.ITokenSourceLocation): IMemberExpression {
+            return this.createNode({
+                type: "MemberExpression",                
+                object,
+                property,
+                computed
+            }, loc);
+        }
+        
+        public createSwitchCase(test: IExpression, consequent: IStatement[], loc?: lexical.ITokenSourceLocation): ISwitchCase {
+            return this.createNode({
+                type: "SwitchCase",                
+                test,
+                consequent
+            }, loc);
+        }
+        
+        public createCatchClause(param: string, body: IBlockStatement, loc?: lexical.ITokenSourceLocation): ICatchClause {
+            return this.createNode({
+                type: "CatchClause",                
+                param,
+                body
+            }, loc);
+        }
+        
+        public createIdentifier(name: string, loc?: lexical.ITokenSourceLocation): IIdentifier {
+            return this.createNode({
+                type: "Identifier",                
+                name
+            }, loc);
+        }
+        
+        public createLiteral(value: string | boolean | number | IRegExp, loc?: lexical.ITokenSourceLocation): ILiteral {
+            return this.createNode({
+                type: "Literal",                
+                value
+            }, loc);
+        }
+       
+    }
+    
+}

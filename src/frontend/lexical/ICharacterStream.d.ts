4dca85bcfa831984b956ab718d514f9b3ed81c3b
Giannhs Apostolidhs <giannhs.apostolidhs@gmail.com>
2016-02-14 01:16:39 +0000

    update documentation in comments

--- src/frontend/lexical/ICharacterStream.d.ts
+++ src/frontend/lexical/ICharacterStream.d.ts
@@ -1,15 +1,40 @@
 declare module trl.frontend.lexical {
 	export interface ICharacterStream {
-		getChar(): number;
-		getNextChar(): number;
-		getCursor(): number;
-		bwdCursor();
-		fwdCursor();
-		bwdCursorRange(range: number);
-		tokenize(startPos: number): string;
-		match(char: number): boolean;
-		matchComplex(comparator: (char: number) => boolean): boolean;
-		isEof(): boolean;
+
+        // returns the current character
+        getChar(): number;
+
+        // returns the next character
+        getNextChar(): number;
+
+        // returns the current position 
+        // of the cursor in the stream
+        getCursor(): number;
+
+        // backward the cursor's position in the stream
+        bwdCursor();
+
+        // forward the cursor's position in the stream
+        fwdCursor();
+
+        // backward the cursor's position in the 
+        // stream for 'range' length of characters
+        bwdCursorRange(range: number);
+
+        // returns the corresponding string from 'startPos'
+        // until the current position of the cursor
+        tokenize(startPos: number): string;
+
+        // forward the cursor's position if the 'char' 
+        // matches the next character
+        match(char: number): boolean;
+
+        // forward the cursor's position if the result of the 
+        // call of the 'comparator' matches the next character
+        matchComplex(comparator: (char: number) => boolean): boolean;
+
+        // returns if the cursor has reach the end of the stream
+        isEof(): boolean;
 	}
 }
 

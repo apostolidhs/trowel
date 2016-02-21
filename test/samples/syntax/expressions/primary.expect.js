4765cd7eb6e32e568919700649df89c6ef48a13b
Giannhs Apostolidhs <giannhs.apostolidhs@gmail.com>
2016-02-21 23:55:35 +0000

    add test for primary expressions (actually only single regex literal :) )

---
+++ test/samples/syntax/expressions/primary.expect.js
@@ -0,0 +1,150 @@
+{
+    "loc": {
+        "start": {
+            "line": 1,
+            "column": 0
+        },
+        "end": {
+            "line": 5,
+            "column": 13
+        }
+    },
+    "type": "Program",
+    "body": [
+        {
+            "loc": {
+                "start": {
+                    "line": 1,
+                    "column": 0
+                },
+                "end": {
+                    "line": 1,
+                    "column": 4
+                }
+            },
+            "type": "ExpressionStatement",
+            "expression": {
+                "loc": {
+                    "start": {
+                        "line": 1,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 1,
+                        "column": 3
+                    }
+                },
+                "type": "Literal",
+                "value": "/a/"
+            }
+        },
+        {
+            "loc": {
+                "start": {
+                    "line": 2,
+                    "column": 0
+                },
+                "end": {
+                    "line": 2,
+                    "column": 7
+                }
+            },
+            "type": "ExpressionStatement",
+            "expression": {
+                "loc": {
+                    "start": {
+                        "line": 2,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 2,
+                        "column": 6
+                    }
+                },
+                "type": "Literal",
+                "value": "/a[b]/"
+            }
+        },
+        {
+            "loc": {
+                "start": {
+                    "line": 3,
+                    "column": 0
+                },
+                "end": {
+                    "line": 3,
+                    "column": 11
+                }
+            },
+            "type": "ExpressionStatement",
+            "expression": {
+                "loc": {
+                    "start": {
+                        "line": 3,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 3,
+                        "column": 10
+                    }
+                },
+                "type": "Literal",
+                "value": "/a[asd]b/i"
+            }
+        },
+        {
+            "loc": {
+                "start": {
+                    "line": 4,
+                    "column": 0
+                },
+                "end": {
+                    "line": 4,
+                    "column": 12
+                } 
+            }, 
+            "type": "ExpressionStatement",
+            "expression": {
+                "loc": {
+                    "start": {
+                        "line": 4,
+                        "column": 0
+                    },
+                    "end": { 
+                        "line": 4,
+                        "column": 11
+                    }
+                },
+                "type": "Literal",
+                "value": "/a[as]d]b/i"
+            }
+        },
+        {
+            "loc": {
+                "start": {
+                    "line": 5,
+                    "column": 0
+                },
+                "end": {
+                    "line": 5,
+                    "column": 13
+                }
+            },
+            "type": "ExpressionStatement",
+            "expression": {
+                "loc": {
+                    "start": {
+                        "line": 5,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 5,
+                        "column": 12
+                    }
+                },
+                "type": "Literal",
+                "value": "/a[as][d]b/g"
+            }
+        }
+    ]
+}
---
+++ test/samples/syntax/expressions/primary.js
@@ -0,0 +1,5 @@
+/a/;
+/a[b]/;
+/a[asd]b/i;
+/a[as]d]b/i;
+/a[as][d]b/g;

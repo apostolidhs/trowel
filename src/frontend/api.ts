b9662d1daf0c5744e4049ac55644c33a935a2e6c
Giannhs Apostolidhs <giannhs.apostolidhs@gmail.com>
2016-02-21 23:57:37 +0000

    export syntax functionality in the API

--- src/frontend/api.ts
+++ src/frontend/api.ts
@@ -1,7 +1,8 @@
 /// <reference path="../../tsDefinitions/tsd.d.ts" />
-/// <reference path="lexical/Lexer.ts" />
 /// <reference path="utilities/Exception.ts" />
+/// <reference path="lexical/Lexer.ts" />
 /// <reference path="lexical/CharacterStream.ts" />
+/// <reference path="syntax/Parser.ts" />
 
 module trl.frontend.api {
 
@@ -9,26 +10,47 @@ module trl.frontend.api {
 		tokens: lexical.IToken[],
 		exceptions?: utilities.IException[]
 	}
+    
+	interface IParserResult {
+		program: any,
+		exceptions?: utilities.IException[]
+	}    
 
 	export function tokenize(src: string, options?: lexical.ILexerOptions): ITokenizeResult {
-		const cs = new lexical.CharacterStream(src),
-			eh = new utilities.ExceptionHandler(),
-			lex = new lexical.Lexer(cs, eh, options);
+		const charStream = new lexical.CharacterStream(src),
+			exceptionHandler = new utilities.ExceptionHandler(),
+			lexer = new lexical.Lexer(charStream, exceptionHandler, options);
 
 		const tokens: lexical.IToken[] = [];
-		while (lex.hasNext()) {
-			const token = lex.nextToken();
+		while (lexer.hasNext()) {
+			const token = lexer.nextToken();
             tokens.push(token);
 		};
 
-		let tokenizeResult: ITokenizeResult = {
+		const tokenizeResult: ITokenizeResult = {
 			tokens: tokens
 		};
 
-		if (eh.hasExceptions()) {
-			tokenizeResult.exceptions = eh.getExceptions();
+		if (exceptionHandler.hasExceptions()) {
+			tokenizeResult.exceptions = exceptionHandler.getExceptions();
 		}
 		return tokenizeResult;
 	}
+    
+    export function parse(src: string, options?: syntax.IParserOptions): IParserResult {
+        const parser = new syntax.Parser(src, options);
+        const parseNodes = parser.parse();
+        
+		const parseResult: IParserResult = {
+			program: parseNodes
+		};
+                
+        const exceptionHandler = parser.getExceptions();
+        
+		if (exceptionHandler.hasExceptions()) {
+			parseResult.exceptions = exceptionHandler.getExceptions();
+		}        
+        return parseResult;
+    }
 
 } 
